6531cc1db8b2253ab79da9430fd7080be616c625
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-01-09 14:00:27 +0100

    Leibnitz and red color

--- nilakantha.cpp
+++ nilakantha.cpp
@@ -8,37 +8,91 @@
 
 
 #include <iostream>
+#include <sstream>
 #include <chrono>
 #include <iomanip>
 #include <cmath>
 #include <limits>
+
+const std::string pi() 
+{
+    std::ostringstream str;
+    str << std::setprecision(std::numeric_limits<long double>::digits10 + 1);
+    str << std::atan(1)*4; 
+    return str.str();
+}
+
+std::string printDecExpColor(double value)
+{
+    const char* CLEAR = "\x1B[0m";
+    const char* RED = "\x1B[31m";
+
+    std::string piStr = pi();
+
+    std::ostringstream str;
+    str << std::setprecision(std::numeric_limits<long double>::digits10 + 1);
+    str << value;
+    std::string valueS = str.str();
+
+    std::ostringstream retStr;
+    retStr << RED;
+    for(std::string::size_type  i = 0; i < piStr.size(); ++i)
+    {
+        if (valueS[i] != piStr[i])
+	{
+	    retStr << CLEAR;
+	}
+	retStr << valueS[i];
+    }
+    retStr << CLEAR;
+
+    std::string retValue = retStr.str();
+    return retValue;
+}
 int main()
 {
     long double decExp{};
-    long double eps = std::numeric_limits<long double>::epsilon() * 10;
-    std::cout << "eps = "<< eps << std::endl;
+    long double decExpLeibnitz{};
+    long double eps = std::numeric_limits<long double>::epsilon() * 100;
+    std::cout << "          eps = "<< eps << std::endl;
 
     std::cout 
         << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
-        << "M_PI ="
-        << "\t"
+        << "         M_PI = "
         << M_PI
+        << std::endl
+        << "    atan(1)*4 = "
+        << pi()
+        << std::endl
+        << std::endl
         << std::endl;
 
     auto begin = std::chrono::high_resolution_clock::now();
-    for (long double i = 0 ; i < 1800000 ; ++i)
+    for (long double i = 0 ; i < 5000000 ; ++i)
     {
         auto now = std::chrono::high_resolution_clock::now();
         long double tmp = pow(-1,i) * 4.0L / ((2*i+2)*(2*i+3)*(2*i+4));
+        long double tmpLeibnitz = pow(-1,i) * 4.0L / (2*i+1);
         auto stoper = std::chrono::duration_cast<std::chrono::seconds>(now - begin);
         std::cout 
+            << "\e[A"
+            << "\e[A"
             << "\r"
             << stoper.count()
             << " sec"
+            << "\n"
+            << "Nilakantha "
             << "\t"
-	        << 3+decExp; 
+	        << printDecExpColor(3+decExp);
+
+        std::cout 
+	    << "\n"
+            << "Leibnitz   "
+            << "\t"
+	        << printDecExpColor(decExpLeibnitz);
 
         decExp += tmp;
+        decExpLeibnitz += tmpLeibnitz;
         if (std::abs(tmp) < eps)
             break;
     }
